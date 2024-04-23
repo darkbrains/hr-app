2f3f8a5c5173507287bd65a0850f88dda3c1a0e5
dark-brains <aram.blackdocs@gmail.com>
2024-04-23 10:52:08 +0400

    final message fix order

--- bin/app/job.py
+++ bin/app/job.py
@@ -1,17 +1,15 @@
 import asyncio
-from utils.email_resend import resend_emails
+from utils.email_resend import resend_emails, update_message_status
 from utils.phone_message_resend import resend_messages
 from utils.logger import logger
 
 
 async def main():
     try:
-        await resend_emails()
-        logger.info('The resend_emails function completed successfully.')
         await resend_messages()
         logger.info('The resend_messages function completed successfully.')
-        update_message_status(email, True)
-        logger.info(f"Update message status for {email}.")
+        await resend_emails()
+        logger.info('The resend_emails function completed successfully.')
     except Exception as e:
         logger.error(f'Unexpected error occurred: {e}')
 
